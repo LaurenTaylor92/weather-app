a7078b7863ac7b6b8c3e1b5c57e12d4025259529
LaurenTaylor92 <118805680+LaurenTaylor92@users.noreply.github.com>
2022-12-29 16:59:38 +0100

    Moved weather app into new file and pushed to GitHub

--- src/script.js
+++ src/script.js
@@ -1,1 +1,81 @@
-alert("");
+let now = new Date();
+let h3 = document.querySelector("h3");
+
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+h3.innerHTML = `${day}, ${date} ${month} ${year}, ${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector(
+    "#city"
+  ).innerHTML = `Weather in ${response.data.name}`;
+  document.querySelector("#temperature").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°C`;
+  document.querySelector(
+    "#humid"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
+    response.data.wind.speed
+  )}km/h`;
+  document.querySelector(
+    "#temp-description"
+  ).innerHTML = `Forecast: ${response.data.weather[0].main}`;
+}
+
+function searchCity(city) {
+  let apiKey = "1714417e6c721ab4697220c1050cd086";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = "1714417e6c721ab4697220c1050cd086";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
